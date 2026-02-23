0983f0bdd8a1961d6e5a632a31fd2200ed533f36
Sidonie Zollikofer <sidonie.zollikofer@studentfr.ch>
2026-02-23 14:09:28 +0100

    troisièmes modifications

--- app.js
+++ app.js
@@ -273,7 +273,8 @@
       // Crée un badge pour chaque type
       types.forEach(function (typeName) {
         const badge = document.createElement("span");
-        badge.className = "type-badge";
+        badge.classList.add("type-badge");
+        badge.classList.add("type-" + typeName.toLowerCase()); // Classe pour le style spécifique au type
         badge.textContent = typeName;
         typesContainer.appendChild(badge);
       });
